62898c9e06db78311871050154b91a40485936a1
lMarinl <marutunan67@gmail.com>
2023-10-28 00:52:19 +0300

    tasks 1 and 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
 
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+
+buttonStart.addEventListener('click', onButtonStart);
+buttonStop.addEventListener('click', onButtonStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+
+function onButtonStart() {
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000
+  );
+ buttonStart.disabled = true;
+};
+
+
+function onButtonStop() {
+  buttonStart.disabled = false;
+  clearInterval(timerId);
+};
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+// import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+
+function onSubmit(event) {
+  event.preventDefault();
+
+const delay = parseInt(form.elements.delay.value);
+const step = parseInt(form.elements.step.value);
+const amount = parseInt(form.elements.amount.value);
+
+
+  for (let i = 1; i <= amount; i += 1) {
+
+ createPromise(i, delay + step * i)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
 }
+};
+
+
+function createPromise(position, delay) {
+
+  const promise = new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+  const obj = {
+    position,
+    delay,
+  };
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(obj)
+      } else {
+        reject(obj)
+      }
+    }, delay);
+    
+    
+  });
+  return promise;
+  
+};
+
